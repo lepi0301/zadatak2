6f2d97691e167388d2f8f444a4db67718d6f9f36
lepi0301 <108347752+lepi0301@users.noreply.github.com>
2022-10-23 23:51:10 +0200

    Add files via upload

---
+++ vijesti.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <meta charset="UTF-8">
+        <meta name="description" content="Vijesti">
+        <meta name="keywords" content="Vijesti, Andrija Jarak">
+        <meta name="author" content="Lepi">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Vijesti </title>
+    </head>
+<body> 
+    <div class="flex-container">
+        
+            <p> Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj
+            i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski 
+            standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo 
+            tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. 
+            Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet 
+            elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. 
+            Postao je popularan tijekom 1960-ih s pojavom Letraset listova s 
+            odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo 
+            kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
+         <img src="https://www.tportal.hr/media/thumbnail/900x540/1720869.jpeg?cropId=2123363"
+        >
+                  
+        </div>  
+
+    <div class="flex-container" :nth-child(2)>
+        
+            <p> Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj
+            i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski 
+            standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo 
+            tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. 
+            Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet 
+            elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. 
+            Postao je popularan tijekom 1960-ih s pojavom Letraset listova s 
+            odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo 
+            kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
+        <img src="https://www.tportal.hr/media/thumbnail/900x540/1720869.jpeg?cropId=2123363"
+    >
+              
+        </div>
+    <div class="flex-container">
+        
+            <p> Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj
+            i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski 
+            standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo 
+            tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. 
+            Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet 
+            elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. 
+            Postao je popularan tijekom 1960-ih s pojavom Letraset listova s 
+            odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo 
+            kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
+         <img src="https://www.tportal.hr/media/thumbnail/900x540/1720869.jpeg?cropId=2123363"
+        >
+                  
+        </div>       
+</body>
+</hmtl>   
